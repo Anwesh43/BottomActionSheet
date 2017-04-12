f6fbf135b254eff7f7dcc1f48324453a0a5059f3
Anwesh43 <anweshthecool0@gmail.com>
2017-04-12 23:12:30 +0530

    Using BottomActionSheet in app

--- app/src/main/java/com/anwesome/ui/bottomactionsheetdemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/bottomactionsheetdemo/MainActivity.java
@@ -2,12 +2,27 @@ package com.anwesome.ui.bottomactionsheetdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.anwesome.ui.bottomactionsheet.ActionSheet;
+import com.anwesome.ui.bottomactionsheet.ActionSheetElement;
 
 public class MainActivity extends AppCompatActivity {
-
+    private String actions[] = {"Delete","Add","Order","Track"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActionSheet actionSheet = new ActionSheet(this);
+        for(int i=0;i<actions.length;i++) {
+            final String action = actions[i];
+            actionSheet.addActionSheetElement(new ActionSheetElement(action, new ActionSheetElement.OnElementClickListener() {
+                @Override
+                public void onElementClick() {
+                    Toast.makeText(MainActivity.this, action , Toast.LENGTH_SHORT).show();
+                }
+            }));
+        }
+        actionSheet.show();
     }
 }
